1c3d9821784328cf28edb7609e802a54f9c8a620
kingpin9292 <jakarea9292@gmail.com>
2026-02-11 21:44:42 -0500

    Implement the complete layout

--- banking_system/app/(root)/layout.tsx
+++ banking_system/app/(root)/layout.tsx
@@ -1,12 +1,22 @@
+import MobileNav from "@/components/ui/MobileNav";
+import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Jakarea", lastName: "Choudhury" };
   return (
-    <main className="bg-black-1">
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={38} height={30} alt="menu icon" />
+          <MobileNav user={loggedIn} />
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- banking_system/app/(root)/page.tsx
+++ banking_system/app/(root)/page.tsx
@@ -1,7 +1,39 @@
+import HeaderBox from "@/components/ui/HeaderBox";
+import RightSidebar from "@/components/ui/RightSidebar";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div className="bg-black-2">Home</div>;
+  const loggedIn = {
+    firstName: "Jakarea",
+    lastName: "Choudhury",
+    email: "xyz@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        Recent transactions
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 123.5 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
